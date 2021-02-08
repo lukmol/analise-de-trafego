aba70955dc7ce0b767b5181150a914ac21cf4625
Lucas Oliveira <lucmoleconomista@gmail.com>
2021-02-07 23:48:15 -0300

    novos comandos

--- script.R
+++ script.R
@@ -1,1 +1,9 @@
 # Script da Analise de dados
+
+
+dados<-read.csv2('Behavior of the urban traffic of the city of Sao Paulo in Brazil.csv')
+pacman::p_load(tidyverse,magrittr,ggplot2)
+
+
+dados %<>% tibble()  %>% rename("lentidao" = Slowness.in.traffic....)
+dados %>% select(lentidao) %>% summarise(media = mean(lentidao))
